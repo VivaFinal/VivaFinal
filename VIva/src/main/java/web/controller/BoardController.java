47cb85428e818a8b302a3669f599a2c5a313dcad
WJ <WJ@DESKTOP-07COT7C>
2023-05-24 16:38:34 +0900

    20230524 우정 오후 4시 38분 커밋

--- VIva/src/main/java/web/controller/BoardController.java
+++ VIva/src/main/java/web/controller/BoardController.java
@@ -1,12 +1,21 @@
 package web.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import web.dto.Board;
+import web.dto.Tag;
 import web.service.face.BoardService;
 import web.util.Paging;
 
@@ -22,9 +31,51 @@ public class BoardController {
 	public void Page( Paging paging, Model model ) {
 		logger.info("/board/list [GET]");
 		
+		//페이징 계산
+		Paging page = boardService.getPaging( paging );
+		logger.info("{}", page);
 		
+		//게시글 목록 조회
+		List<Board> boardList = boardService.list( page );
 		
+		model.addAttribute("page", page);
+		model.addAttribute("boardList", boardList);
+	}
+	
+	@RequestMapping("/view")
+	public String view( Board viewBoard, Model model ) {
+		logger.info("/board/view");
 		
+		//잘못된 게시글 번호 처리
+		if( viewBoard.getBoardNo() < 1 ) {
+			return "redirect:./list";
+		}
+		
+		//게시글 조회
+		viewBoard = boardService.view( viewBoard );
+		logger.info("조회된 게시글 {}", viewBoard);
+		
+		//모델값 전달  - 게시글
+		model.addAttribute("viewBoard", viewBoard);
+		
+		return "board/view";
+	
+	}
+		
+	
+	@GetMapping("/write")
+	public void write() {}
+	
+	@PostMapping("/write")
+	public String writeProc( Board board, MultipartFile file, HttpSession session ) {
+		logger.info("/board/write [POST]");
+		
+		board.setUserId( (String) session.getAttribute("id") );
+		board.setUserNick( (String) session.getAttribute("nick") );
+		
+		boardService.write( board, file );
+		
+		return "redirect:./list";	//게시글 목록
 	}
 
 }
--- VIva/src/main/webapp/WEB-INF/views/board/list.jsp
+++ VIva/src/main/webapp/WEB-INF/views/board/list.jsp
@@ -2,24 +2,15 @@
     pageEncoding="UTF-8"%> 
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>게시판</title>
+<c:import url="/WEB-INF/views/layout/header.jsp" />
+
 
 <style type="text/css">
 
-.head {
-	font-family:"typeFont";
-	 src: url('../../resources/font/BalooBhaina2-VariableFont_wght.ttf')
+.head, .body {
+	 font-family: "typeFont"; 
+    src: url('../../resources/font/BalooBhaina2-VariableFont_wght.ttf') 
 	text-align: center; 
-}
-
-.body {
-	font-family:"typeFont";
-	 src: url('../../resources/font/BalooBhaina2-VariableFont_wght.ttf')
-	text-align: center;
 }
 
 </style>
@@ -38,26 +29,24 @@
 <thead>
 <tr>
 	<th>게시글 번호</th>
-	<th>게시글 제목</th>
 	<th>카테고리</th>
-	<th>작성자 닉네임</th>
+	<th>게시글 제목</th>
 	<th>조회수</th>
 	<th>게시글 작성일</th>
 </tr>
 </thead>
 
-<c:forEach var="list" items="${list}">
 <tbody>
+<c:forEach var="boardList" items="${boardList}">
 <tr>
-	<td>${list.boardNo }</td>
-	<td>${list.boardTitle }</td>
-	<td>${list.categoryType }</td>
-	<td>${list.userNick }</td>
-	<td>${list.boardHit }</td>
-	<td>${list.boardDate }</td>
+	<td>${boardList.boardNo }</td>
+	<td>${boardList.categoryType }</td>
+	<td>${boardList.boardTitle }</td>
+	<td>${boardList.boardHit }</td>
+	<td>${boardList.boardDate }</td>
 </tr>
-</tbody>
 </c:forEach>
+</tbody>
 </table>
 </div>
 
