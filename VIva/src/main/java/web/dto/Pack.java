2231c2266ed29a6e8b792bee7700dafd9e710d00
Rivershot <pcw4470@hanmail.net>
2023-05-24 16:35:37 +0900

    <230524_태그 구현 중_박채원>

--- VIva/src/main/java/web/dto/Pack.java
+++ VIva/src/main/java/web/dto/Pack.java
@@ -7,28 +7,24 @@ public class Pack {
 private int packNo;       
 private String packName;     
 private String packContent;  
-private String packImg;      
 private Date packDate;     
 private int tagNo;        
-private int sourceNo;     
 
 public Pack() {}
 
-public Pack(int packNo, String packName, String packContent, String packImg, Date packDate, int tagNo, int sourceNo) {
+public Pack(int packNo, String packName, String packContent, Date packDate, int tagNo) {
 	super();
 	this.packNo = packNo;
 	this.packName = packName;
 	this.packContent = packContent;
-	this.packImg = packImg;
 	this.packDate = packDate;
 	this.tagNo = tagNo;
-	this.sourceNo = sourceNo;
 }
 
 @Override
 public String toString() {
-	return "Pack [packNo=" + packNo + ", packName=" + packName + ", packContent=" + packContent + ", packImg=" + packImg
-			+ ", packDate=" + packDate + ", tagNo=" + tagNo + ", sourceNo=" + sourceNo + "]";
+	return "Pack [packNo=" + packNo + ", packName=" + packName + ", packContent=" + packContent + ", packDate="
+			+ packDate + ", tagNo=" + tagNo + "]";
 }
 
 public int getPackNo() {
@@ -55,14 +51,6 @@ public void setPackContent(String packContent) {
 	this.packContent = packContent;
 }
 
-public String getPackImg() {
-	return packImg;
-}
-
-public void setPackImg(String packImg) {
-	this.packImg = packImg;
-}
-
 public Date getPackDate() {
 	return packDate;
 }
@@ -79,13 +67,7 @@ public void setTagNo(int tagNo) {
 	this.tagNo = tagNo;
 }
 
-public int getSourceNo() {
-	return sourceNo;
-}
 
-public void setSourceNo(int sourceNo) {
-	this.sourceNo = sourceNo;
-}
 
 
 
--- VIva/src/main/java/web/service/impl/SourceServiceImpl.java
+++ VIva/src/main/java/web/service/impl/SourceServiceImpl.java
@@ -1,5 +1,9 @@
 package web.service.impl;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +19,15 @@ public class SourceServiceImpl implements SourceService{
 	@Autowired SourceDao sourceDao;
 	
 	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Override
+	public List<Tag> getTag(String genre) {
+		logger.info("getTag() Service 확인용");
+		
+		List<Tag> tag = sourceDao.selectTagByGenre(genre);
+		
+		return tag;
+	}
 
 	
 	
